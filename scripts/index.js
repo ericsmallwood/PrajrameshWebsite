18cc82b8fd1af0604387a9e23e60e36e1ea4d973
unknown <esmallwood@SW221749.ellis.com>
2015-07-30 12:15:46 -0500

    Added flag section, and completed banner scroller.

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by esmallwood on 7/30/2015.
+ */
+$('.bxslider').bxSlider({
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 100,
+    slideMargin: 120,
+    moveSlides: 1
+});
+
+$('.bx-prev').css("left", "-10%");
+$('.bx-next').css("right", "-10%");
