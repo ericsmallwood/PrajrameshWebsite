a80ae6dee38b0ae920c4fd2fe03691fd8e3f30ab
Eric Smallwood <smallwood.eric@ymail.com>
2015-08-01 12:38:30 -0500

    Ads Across

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,20 @@
 /**
  * Created by esmallwood on 7/30/2015.
  */
-$('.bxslider').bxSlider({
+$('#adsAcrossSlider').bxSlider({
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 200,
+    slideMargin: 120,
+    moveSlides: 1,
+    speed: 200,
+    auto: true,
+    pause: 2000,
+    pager: false
+
+});
+
+$('#flagsSlider').bxSlider({
     minSlides: 4,
     maxSlides: 4,
     slideWidth: 100,
